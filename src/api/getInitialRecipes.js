8ba63152c376158ba77b7b1bb77153bb47d286f4
BrandtLC <lucasbrandtcosta@gmail.com>
2022-08-23 11:21:16 -0300

    cria requisição inicial à api

---
+++ src/api/getInitialRecipes.js
@@ -0,0 +1,21 @@
+const getInitialRecipes = async (pathname) => {
+  try {
+    const END_POINT_FOODS = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+    const END_POINT_DRINKS = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+    let apiResponse;
+    if (pathname === '/foods') {
+      apiResponse = await fetch(END_POINT_FOODS)
+        .then((response) => response.json());
+      return (apiResponse);
+    } if (pathname === '/drinks') {
+      apiResponse = await fetch(END_POINT_DRINKS)
+        .then((response) => response.json());
+      return (apiResponse);
+    }
+    throw new Error('Pathname inválido');
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export default getInitialRecipes;
