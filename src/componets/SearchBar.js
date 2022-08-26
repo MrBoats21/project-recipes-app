9ac73d786de8b7f49a35fdb822a441e038ffb082
BrandtLC <lucasbrandtcosta@gmail.com>
2022-08-26 16:55:20 -0300

    comitei

--- src/componets/SearchBar.js
+++ src/componets/SearchBar.js
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+
+import { useHistory } from 'react-router-dom';
+import recipeContext from '../contex/recipeContext';
 
 function SearchBar() {
+  const history = useHistory();
+  const { setApiResponse } = useContext(recipeContext);
+  const [search, setSearch] = useState('');
+  const [searchType, setSearchType] = useState('');
   return (
-    <h1>SearchBar</h1>
+    <>
+      <input
+        type="text"
+        data-testid="search-input"
+        value={ search }
+        onChange={ (event) => setSearch(event.target.value) }
+      />
+      <label htmlFor="name">
+        <input
+          type="radio"
+          id="name"
+          name="searchRadio"
+          onChange={ (event) => setSearchType(event.target.id) }
+        />
+        Nome
+      </label>
+
+      <label htmlFor="Ingredient">
+        <input
+          type="radio"
+          id="Ingredient"
+          name="searchRadio"
+          onChange={ (event) => setSearchType(event.target.id) }
+        />
+        Ingrediente
+      </label>
+
+      <label htmlFor="first_letter">
+        <input
+          type="radio"
+          id="first_letter"
+          name="searchRadio"
+          onChange={ (event) => setSearchType(event.target.id) }
+        />
+        Primiera Letra
+      </label>
+    </>
   );
 }
 
